e5cd989841cb29f7322423074b5df4bea8eca9bb
APURV CHOUBEY <apurv.main2@gmail.com>
2022-05-22 20:00:23 +0530

    Added floating cards

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+console.log("Hello world");
+
+
+/*
+class="focus:outline-none text-white bg-red-500 text-2xl px-7 py-2 rounded-full hover:bg-black hover:border-blue-300 hover:border-2"
+
+class="focus:outline-none text-white bg-black text-2xl px-7 py-2 rounded-full border-blue-300 border-2 hover:bg-red-500 hover:border-black hover:border-2"
+
+*/
